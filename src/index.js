818285bd23ef579b2179f3e94958e17e52726685
HuiArroheily <hui.wu@student.com>
2021-05-10 17:45:12 +0800

    feat: Loading CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,20 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    // filename: '[name].[hash:8].js',
     path: path.resolve(__dirname, 'dist'),
     filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
